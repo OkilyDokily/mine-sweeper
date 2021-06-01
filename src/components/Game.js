5aeabcf835aadc05650e5091ee1629f331f80e97
me <me@me.me.com>
2021-06-01 14:02:25 -0700

    removed props from game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Controls from './Controls'
 import MinesweeperField from './MinesweeperField'
 
@@ -10,10 +9,6 @@ const Game = props => {
       <MinesweeperField/>
     </div>
   );
-};
-
-Game.propTypes = {
-  
 };
 
 export default Game;
