3c291c41013ead31286844314d74c21287069d68
me <me@me.me.com>
2021-06-01 14:00:26 -0700

    update rules

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <div id="instructions">
         <p>Click on a tile to get started.</p>
         <p>Right click to add a flag.</p>
+        <p>Each mine must be flagged for you to win the game</p>
         <p>Avoid the bombs.</p>
         <p>The number tells you how many bombs are nereby.</p>
         <p>No mines ever on the first click</p>
